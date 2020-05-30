0b9a869a6e0eea037434d60cf956cab32c23f373
consumer-git <59201447+consumer-git@users.noreply.github.com>
2020-05-30 15:02:05 +0530

    Update pythontry.py

--- pythontry.py
+++ pythontry.py
@@ -5,14 +5,19 @@ import time
 cpu_number=random.randint(1,10)
 
 while True:
+
+          time.sleep(1)
+
           welcome_note=("welcome to 'guess the number' \n I'm thinking of a number from 1 to 10. \n Can you guess?")
           print(welcome_note)
-          user_num=input('so tell me what number am I thinking of from 1 to 10? \n tell me! \n or press q to quit trying ')
+          time.sleep ( 1 )
+          user_num=input('\n so tell me what number am I thinking of from 1 to 10? \n tell me! \n or press q to quit trying ')
 
           if user_num==cpu_number:
               print('congrats {} is the right answer'.format(cpu_number))
 
           elif user_num!=cpu_number:
+              time.sleep ( 0.5 )
               print('OOPS! that is not the right answer! \n MY number was {} \n try again!'.format(cpu_number))
 
 
